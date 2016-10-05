de7cba130c518954dbd7bcefcb4a060278beba13
Pascal Zingg <pazingg@gmail.com>
2016-10-05 13:23:34 +0200

    Adds improved plots for unmodified and lowercase text

--- exercises/exercise2/counter.sh
+++ exercises/exercise2/counter.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-tr -c '[:alnum:]' '[\n*]' < pg1524.txt | sort | uniq -c | sort -nr | head -30 | tail -29 > out.dat
-gnuplot -p -e "plot 'out.dat' using 1:xticlabels(2) with boxes"
+tr -c '[:alnum:]' '[\n*]' < hamlet.txt | sort | uniq -c | sort -nr | head -30 | tail -29 > unmodified.dat
+tr -c '[:alnum:]' '[\n*]' < hamlet.txt | egrep "^[[:lower:]]+$" | sort | uniq -c | sort -nr | head -30 | tail -29 > lowercase.dat
+
+# TODO: Stopwords
+# TODO: Stemmed text
+
+general_gnuplot="set style fill solid border -1; set xtics rotate;"
+gnuplot -p -e "$general_gnuplot set title 'Unmodified text'; plot 'unmodified.dat' using 1:xticlabels(2) with boxes"
+gnuplot -p -e "$general_gnuplot set title 'Lowercase words'; plot 'lowercase.dat' using 1:xticlabels(2) with boxes"
 
