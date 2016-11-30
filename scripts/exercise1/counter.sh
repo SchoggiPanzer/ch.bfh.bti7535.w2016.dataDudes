a4cad97b28152638e3a937fd7dbb582a61f70bfc
Pascal Zingg <pazingg@gmail.com>
2016-11-30 16:03:58 +0100

    Prepares repo for sentimental analysis project

--- scripts/exercise1/counter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tr -c '[:alnum:]' '[\n*]' < hamlet.txt | sort | uniq -c | sort -nr | head -30 | tail -29 > tfd.out
-gnuplot -p -e "plot 'out.dat' using 1:xticlabels(2) with boxes"
-
-rm *.out
-
--- scripts/exercise2/tokenizer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-content=`< $1`
-IFS=' '; 
-for word in $content; do 
-    echo "$word"
-done
