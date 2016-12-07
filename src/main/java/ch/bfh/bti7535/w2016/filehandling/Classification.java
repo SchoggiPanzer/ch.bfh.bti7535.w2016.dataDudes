0f732b719f14416f1f82990ee37741df5a5ffccf
Pascal Zingg <pazingg@gmail.com>
2016-12-07 14:22:33 +0100

    Improves classification enum

--- src/main/java/ch/bfh/bti7535/w2016/filehandling/Classification.java
+++ src/main/java/ch/bfh/bti7535/w2016/filehandling/Classification.java
@@ -1,6 +1,7 @@
 package ch.bfh.bti7535.w2016.filehandling;
 
 public enum Classification {
-	POSITIVE,
-	NEGATIVE
+	SENTIMENT_POSITIVE,
+	SENTIMENT_NEGATIVE,
+	NOT_CLASSIFIED
 }
---
+++ src/test/java/ch/bfh/bti7535/w2016/algorithm/BaselineAlgorithmTest.java
@@ -0,0 +1,35 @@
+package ch.bfh.bti7535.w2016.algorithm;
+
+import ch.bfh.bti7535.w2016.filehandling.Classification;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BaselineAlgorithmTest {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testExecute() throws Exception {
+
+		Map<String, Document.WordProperty> dummyWords = new HashMap<>();
+		dummyWords.put("wonderful", new Document.WordProperty(1, Classification.SENTIMENT_POSITIVE));
+		dummyWords.put("awesome", new Document.WordProperty(10, Classification.SENTIMENT_POSITIVE));
+		dummyWords.put("terrible", new Document.WordProperty(3, Classification.SENTIMENT_NEGATIVE));
+
+		Document doc = new Document();
+		doc.setContent(dummyWords);
+		List docList = new ArrayList();
+		docList.add(doc);
+
+		BaselineAlgorithm ba = new BaselineAlgorithm();
+		List results = ba.execute(docList);
+	}
+}
