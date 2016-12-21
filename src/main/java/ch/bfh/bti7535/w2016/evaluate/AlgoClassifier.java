092a4bff1524335cde057e2a1893317f99b8649a
Pascal Schmalz <pascal.schmalz@students.bfh.ch>
2016-12-21 13:34:14 +0100

    added AlgoClassifierTests

--- src/main/java/ch/bfh/bti7535/w2016/evaluate/AlgoClassifier.java
+++ src/main/java/ch/bfh/bti7535/w2016/evaluate/AlgoClassifier.java
@@ -1,6 +1,6 @@
 package ch.bfh.bti7535.w2016.evaluate;
-
 import ch.bfh.bti7535.w2016.algorithm.AbstractAlgorithm;
+import ch.bfh.bti7535.w2016.algorithm.BaselineAlgorithm;
 import ch.bfh.bti7535.w2016.algorithm.Document;
 import ch.bfh.bti7535.w2016.filehandling.Classification;
 import ch.bfh.bti7535.w2016.filehandling.FileReader;
@@ -8,83 +8,151 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 public class AlgoClassifier {
 
-	private double truePos = 0;
-	private double trueNeg = 0;
-	private double falsePos = 0;
-	private double falseNeg = 0;
+    private double truePos = 0;
+    private double trueNeg = 0;
+    private double falsePos = 0;
+    private double falseNeg = 0;
 
-	private String path = "./src/main/resources/review_polarity/txt_sentoken";
-	private static Logger log = (Logger) LoggerFactory.getLogger(AlgoClassifier.class);
+    private double delta = 0.0001;
+    private double verbosity = 10;
 
-	public AlgoClassifier(AbstractAlgorithm algo) {
-		//getArrayOfDocs
-		//TODO interface algo
-		FileReader fileReader = new FileReader();
-		try {
-			List<Document> inputdocs = fileReader.readAllFiles(path);
-			List<Document> docs = algo.execute(inputdocs);
-			classify(docs);
-		} catch (FileNotFoundException e) {
-			log.error(e.getMessage(), e);
-		}
-	}
+    private String path = "./src/main/resources/review_polarity/txt_sentoken";
+    private static Logger log = (Logger) LoggerFactory.getLogger(AlgoClassifier.class);
 
-	private void classify(List<Document> docs) {
-		for (Document doc : docs) {
-			classifySingleDoc(doc);
-		}
-	}
 
-	private void classifySingleDoc(Document doc) {
-		truePos = 0;
-		trueNeg = 0;
-		falsePos = 0;
-		falseNeg = 0;
+    public AlgoClassifier(AbstractAlgorithm algo) {
+        FileReader fileReader = new FileReader();
+        try {
+            List<Document> inputdocs = fileReader.readAllFiles(path);
 
-		Classification gold = doc.getGoldStandard();
-		Classification calculated = doc.getTestResult();
-		//Does handle  notclassified as sentiment_negative
-		if (gold == calculated) {
-			//true
-			if (gold == Classification.SENTIMENT_POSITIVE) {
-				truePos++;
-			} else {
-				trueNeg++;
-			}
-		} else {
-			//false
-			if (calculated == Classification.SENTIMENT_POSITIVE) {
-				falsePos++;
-			} else {
-				falseNeg++;
-			}
-		}
-	}
+            List<Document> docs = algo.execute(inputdocs);
+            classify(docs);
+        } catch (FileNotFoundException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 
-	public double getPrecision() {
-		return truePos / (truePos + falsePos);
-	}
+    public AlgoClassifier(AbstractAlgorithm algo, List<Document> inputdocs){
+        List<Document> docs = algo.execute(inputdocs);
+        classify(docs);
 
-	public double getRecall() {
-		return truePos / (truePos + falseNeg);
-	}
+    }
 
-	public double getFmesure() {
-		return getFmesure(1.0);
-	}
 
-	public double getFmesure(double beta) {
-		double precision = this.getPrecision();
-		double recall = this.getRecall();
-		return getFmesure(beta, precision, recall);
-	}
+    protected void classify(List<Document> docs) {
+        truePos = 0;
+        trueNeg = 0;
+        falsePos = 0;
+        falseNeg = 0;
+        for (Document doc : docs) {
+            classifySingleDoc(doc);
+        }
+    }
 
-	public double getFmesure(double beta, double precision, double recall) {
-		return ((1 + beta * beta) * (precision * recall)) / ((beta * beta * precision) + recall);
-	}
+    private void classifySingleDoc(Document doc) {
 
+        Classification gold = doc.getGoldStandard();
+        Classification calculated = doc.getTestResult();
+        //Does handle  notclassified as sentiment_negative
+        if (gold == calculated) {
+            //true
+            if (gold == Classification.SENTIMENT_POSITIVE) {
+                truePos++;
+            } else {
+                trueNeg++;
+            }
+        } else {
+            //false
+            if (calculated == Classification.SENTIMENT_POSITIVE) {
+                falsePos++;
+            } else {
+                falseNeg++;
+            }
+        }
+    }
+
+    public double getPrecision() {
+        double r = 0.0;
+        if (truePos + falsePos < delta) {
+            r = 0.0;
+        } else {
+            r = truePos / (truePos + falsePos);
+        }
+
+        if (verbosity > 1) {
+            System.out.println("");
+            System.out.println("AlgoClassifier:");
+            System.out.println("precision=" + r);
+        }
+
+        if (verbosity > 10) {
+            System.out.println("truePos=" + truePos);
+            System.out.println("trueNeg=" + trueNeg);
+            System.out.println("falsePos=" + falsePos);
+            System.out.println("falseNeg=" + falseNeg);
+        }
+        return r;
+    }
+
+
+    public double getRecall() {
+        double r = 0.0;
+        if (truePos + falseNeg < delta) {
+            r = 0.0;
+        } else {
+            r = truePos / (truePos + falseNeg);
+        }
+        return r;
+    }
+
+    public double getFmesure() {
+        return getFmesure(1.0);
+    }
+
+    public double getFmesure(double beta) {
+        double precision = this.getPrecision();
+        double recall = this.getRecall();
+        return getFmesure(beta, precision, recall);
+    }
+
+    public double getFmesure(double beta, double precision, double recall) {
+        double r;
+        double denominator = (beta *beta * precision) + recall;
+        //odo test
+        if (beta< delta ) {
+            if(beta !=0) {
+                throw new IllegalArgumentException("beta can not be belwo 0");
+            }
+        }
+        if (Math.abs(denominator) < delta) {
+            r = 0;
+        } else {
+            r = ((1 + beta * beta) * ((precision * recall)) / denominator);
+        }
+        return r;
+    }
+
+    public double getAccuracy() {
+        return this.getAccuracy(truePos,trueNeg,falsePos,falseNeg);
+    }
+
+
+    private double getAccuracy(double truePos, double trueNeg, double falsePos, double falseNeg) {
+        double r;
+        double denominator = truePos + trueNeg + falsePos + falseNeg;
+        if (denominator < delta) {
+            r = 0;
+        } else {
+            r = truePos / denominator;
+        }
+        return r;
+    }
 }
+
--- src/test/java/ch/bfh/bti7535/w2016/evaluate/AlgoClassifierTest.java
+++ src/test/java/ch/bfh/bti7535/w2016/evaluate/AlgoClassifierTest.java
@@ -1,34 +1,258 @@
 package ch.bfh.bti7535.w2016.evaluate;
 
+import ch.bfh.bti7535.w2016.algorithm.Document;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by pascal on 15.12.16.
  */
+
 public class AlgoClassifierTest {
+    private double delta = 0.0001;
+    private int times = 10;
+    private List<Document> l  = new ArrayList();
+
+    private DummyAlgo tp = new DummyAlgo();
+    private DummyAlgo tn = new DummyAlgo();
+    private DummyAlgo fp = new DummyAlgo();
+    private DummyAlgo fn = new DummyAlgo();
+    private DummyAlgo mixed = new DummyAlgo();
+
+    private AlgoClassifier tpA;
+    private AlgoClassifier tnA;
+    private AlgoClassifier fpA;
+    private AlgoClassifier fnA;
+    //private AlgoClassifier mixedA;
+    private DummyAlgoClassifier mixedA;
+    /*
+    private List<Document> tpL  = new ArrayList();
+    private List<Document> tnL = new ArrayList();
+    private List<Document> fpL  = new ArrayList();
+    private List<Document> fnL= new ArrayList();
+    private List<Document> mixedL= new ArrayList();
+    */
+
+
+    private ArrayList<Document> results = new ArrayList();
+    private List<Document> r = new ArrayList<>();
+    @Before
+    public void initialize(){
+        tp.addTruePos(times);
+        tn.addTrueNeg(times);
+        fp.addFalsePos(times);
+        fn.addFalseNeg(times);
+
+        mixed.addTruePos(times);
+        mixed.addTrueNeg(times);
+        mixed.addFalsePos(times);
+        mixed.addFalseNeg(times);
+
+        tpA = new AlgoClassifier(tp);
+        tnA = new AlgoClassifier(tn);
+        fpA = new AlgoClassifier(fp);
+        fnA = new AlgoClassifier(fn);
+        mixedA = new DummyAlgoClassifier(mixed);
+
+        /*
+         tpL = tp.execute(l);
+         tnL = tn.execute(l);
+         fpL = fp.execute(l);
+         fnL = fn.execute(l);
+         mixedL = mixed.execute(l);
+         */
+    }
+
+    private double getPrecision(AlgoClassifier algoClassifier){
+        double r= algoClassifier.getPrecision();
+        return r;
+    }
     @Test
-    public void getPrecision() throws Exception {
+    public void getPrecisionTest() throws Exception {
+
+        double tpPrecision =  this.getPrecision(tpA);
+        double tnPrecision =  this.getPrecision(tnA);
+        double fpPrecision =  this.getPrecision(fpA);
+        double fnPrecision =  this.getPrecision(fnA);
+        double mixedPrecision=  this.getPrecision(mixedA);
+
+        assertEquals(1,tpPrecision,delta);
+        assertEquals(0,tnPrecision,delta);
+        assertEquals(0,fpPrecision,delta);
+        assertEquals(0,fnPrecision,delta);
+        assertEquals(0.5,mixedPrecision,delta);
+
+
+
+
+    }
+    private double getRecall(AlgoClassifier algoClassifier){
+        double r= algoClassifier.getRecall();
+        return r;
+
+    }
+    @Test
+    public void getRecallTest() throws Exception {
+        double tpRecall = this.getRecall(tpA);
+        double tnRecall= this.getRecall(tnA);
+        double fpRecall = this.getRecall(fpA);
+        double fnRecall = this.getRecall(fnA);
+        double mixedRecall = this.getRecall(mixedA);
+
+        assertEquals(1,tpRecall,delta);
+        assertEquals(0,tnRecall,delta);
+        assertEquals(0,fpRecall,delta);
+        assertEquals(0,fnRecall,delta);
+        assertEquals(0.5,mixedRecall,delta);
+
+
+    }
+    private double getFmesure(AlgoClassifier algoClassifier){
+        double r = algoClassifier.getFmesure();
+        return r;
+    }
+
+    private double getFmesureBeta(AlgoClassifier algoClassifier,double beta){
+        double r = algoClassifier.getFmesure(beta);
+        return r;
+    }
+
+    @Test
+    public void getFmesureTest() throws Exception {
+        double tpFmesure= this.getFmesure(tpA);
+        double tnFmesure= this.getFmesure(tnA);
+        double fpFmesure= this.getFmesure(fpA);
+        double fnFmesure= this.getFmesure(fnA);
+        double mixedFmesure= this.getFmesure(mixedA);
+
+        assertEquals(1,tpFmesure,delta);
+        assertEquals(0,tnFmesure,delta);
+        assertEquals(0,fpFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(0.5,mixedFmesure,delta);
 
     }
 
     @Test
-    public void getRecall() throws Exception {
+    public void getFmesure1Test() throws Exception {
+        double beta = 3.0;
+        double tpFmesure= this.getFmesureBeta(tpA,beta);
+        double tnFmesure= this.getFmesureBeta(tnA,beta);
+        double fpFmesure= this.getFmesureBeta(fpA,beta);
+        double fnFmesure= this.getFmesureBeta(fnA,beta);
+        double mixedFmesure= this.getFmesureBeta(mixedA,beta);
 
+        assertEquals(1,tpFmesure,delta);
+        assertEquals(0,tnFmesure,delta);
+        assertEquals(0,fpFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(0.5,mixedFmesure,delta);
+
+        /*
+        beta = 4.0;
+        tpFmesure= this.getFmesureBeta(tpA,beta);
+        tnFmesure= this.getFmesureBeta(tnA,beta);
+        fpFmesure= this.getFmesureBeta(fpA,beta);
+        fnFmesure= this.getFmesureBeta(fnA,beta);
+        mixedFmesure= this.getFmesureBeta(mixedA,beta);
+
+        assertEquals(1,tpFmesure,delta);
+        assertEquals(0,tnFmesure,delta);
+        assertEquals(0,fpFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(0.5,mixedFmesure,delta);
+
+        */
+        beta = 0.0;
+        tpFmesure= this.getFmesureBeta(tpA,beta);
+        tnFmesure= this.getFmesureBeta(tnA,beta);
+        fpFmesure= this.getFmesureBeta(fpA,beta);
+        fnFmesure= this.getFmesureBeta(fnA,beta);
+        mixedFmesure= this.getFmesureBeta(mixedA,beta);
+
+        assertEquals(1.0,tpFmesure,delta);
+        assertEquals(0,tnFmesure,delta);
+        assertEquals(0,fpFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(0.5,mixedFmesure,delta);
+
+        /*
+        double tpFmesure= this.getFmesure(tpA);
+        double tnFmesure= this.getFmesure(tnA);
+        double fpFmesure= this.getFmesure(fpA);
+        double tpFmesure= this.getFmesure(tpA);
+        double tnFmesure= this.getFmesure(tnA);
+        double fpFmesure= this.getFmesure(fpA);
+        double fnFmesure= this.getFmesure(fnA);
+        double mixedFmesure= this.getFmesure(mixedA);
+
+        assertEquals(1,tpFmesure,delta);
+        assertEquals(0,tnFmesure,delta);
+        assertEquals(0,fpFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(0.5,mixedFmesure,delta);
+        double fnFmesure= this.getFmesure(fnA);
+        double mixedFmesure= this.getFmesure(mixedA);
+
+        assertEquals(1,tpFmesure,delta);
+        assertEquals(0,tnFmesure,delta);
+        assertEquals(0,fpFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(0.5,mixedFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(0.5,mixedFmesure,delta);
+
+        beta = 0.5;
+        tpFmesure= this.getFmesureBeta(tpA,beta);
+        tnFmesure= this.getFmesureBeta(tnA,beta);
+        fpFmesure= this.getFmesureBeta(fpA,beta);
+        fnFmesure= this.getFmesureBeta(fnA,beta);
+        mixedFmesure= this.getFmesureBeta(mixedA,beta);
+
+        assertEquals(0.83333333333,tpFmesure,delta);
+        assertEquals(0,tnFmesure,delta);
+        assertEquals(0,fpFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(0.41666666,mixedFmesure,delta);
+
+        beta = -2.0;
+        tpFmesure= this.getFmesureBeta(tpA,beta);
+        tnFmesure= this.getFmesureBeta(tnA,beta);
+        fpFmesure= this.getFmesureBeta(fpA,beta);
+        fnFmesure= this.getFmesureBeta(fnA,beta);
+        mixedFmesure= this.getFmesureBeta(mixedA,beta);
+
+        assertEquals(-5,tpFmesure,delta);
+        assertEquals(0,tnFmesure,delta);
+        assertEquals(0,fpFmesure,delta);
+        assertEquals(0,fnFmesure,delta);
+        assertEquals(-2.5,mixedFmesure,delta);
+
+*/
     }
-
+    private double getAccuracy(AlgoClassifier algoClassifier) {
+        double r = algoClassifier.getAccuracy();
+        return r;
+    }
     @Test
-    public void getFmesure() throws Exception {
+    public void getAccuracyTest(){
+        double t1 = tpA.getAccuracy();
+        double t2 = tnA.getAccuracy();
+        double t3 = fpA.getAccuracy();
+        double t4 = fnA.getAccuracy();
+        double t5 = mixedA.getAccuracy();
 
+        assertEquals(1,t1,delta);
+        assertEquals(0,t2,delta);
+        assertEquals(0,t3,delta);
+        assertEquals(0,t4,delta);
+        assertEquals(.25,t5,delta);
     }
-
-    @Test
-    public void getFmesure1() throws Exception {
-
-    }
-
-    @Test
-    public void getFmesure2() throws Exception {
-
-    }
-
 }
