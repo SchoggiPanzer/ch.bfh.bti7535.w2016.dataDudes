abb64ceddec7f1cfdbfaf6ebb335f1b364aac24c
Pascal Zingg <pazingg@gmail.com>
2017-01-04 11:38:55 +0100

    Improves algorithm switching

--- src/main/java/ch/bfh/bti7535/w2016/evaluate/Evaluate.java
+++ src/main/java/ch/bfh/bti7535/w2016/evaluate/Evaluate.java
@@ -1,7 +1,7 @@
 package ch.bfh.bti7535.w2016.evaluate;
 
 import ch.bfh.bti7535.w2016.algorithm.AbstractAlgorithm;
-import ch.bfh.bti7535.w2016.algorithm.BaselineAlgorithm;
+import ch.bfh.bti7535.w2016.algorithm.NaiveBayesAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,20 +9,29 @@ public class Evaluate {
 
 	private static Logger log = (Logger) LoggerFactory.getLogger(Evaluate.class);
 
+	private static void evaluateByClassname(Class clazz) {
+		try {
+			AbstractAlgorithm algo = (AbstractAlgorithm) clazz.newInstance();
+			AlgoClassifier classifier = new AlgoClassifier(algo);
+
+			double fmesure = classifier.getFmesure();
+			double precision = classifier.getPrecision();
+			double recall = classifier.getRecall();
+
+			String baseline = String.format(
+					"\n---------------------\nBaseline Algorithm\n---------------------\n"
+							+ "Precision:\t%f\n"
+							+ "Recall:\t\t%f\n"
+							+ "f-Measure:\t%f\n\n", precision, recall, fmesure);
+
+			log.info(baseline);
+		} catch (IllegalAccessException | InstantiationException e) {
+			log.error("Cannot instantiate {}", clazz.getName());
+		}
+	}
+
 	public static void main(String[] args) {
-		AbstractAlgorithm algo = new BaselineAlgorithm();
-		AlgoClassifier classifier = new AlgoClassifier(algo);
-
-		double fmesure = classifier.getFmesure();
-		double precision = classifier.getPrecision();
-		double recall = classifier.getRecall();
-
-		String baseline = String.format(
-				"Baseline Algorithm\n---------------------\n"
-						+ "Precision:\t%f\n"
-						+ "Recall:\t%f\n"
-						+ "f-Measure:\t%f\n", precision, recall, fmesure);
-
-		log.info(baseline);
+		//evaluateByClassname(BaselineAlgorithm.class);
+		evaluateByClassname(NaiveBayesAlgorithm.class);
 	}
 }
