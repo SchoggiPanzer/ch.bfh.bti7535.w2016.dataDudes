7c0a75fe92236c7e56579cc402b6d594f5ef62f3
Pascal Schmalz <pascal.schmalz@students.bfh.ch>
2017-01-11 14:36:44 +0100

    added CountWordFeaturadded CountWordFeaturee

---
+++ src/main/java/ch/bfh/bti7535/w2016/algorithm/features/WordCount.java
@@ -0,0 +1,14 @@
+/*
+* Copyright (c) 2017 Berner Fachhochschule, Switzerland.
+*
+* Project Smart Reservation System.
+*
+* Distributable under GPL license. See terms of license at gnu.org.
+*/
+package ch.bfh.bti7535.w2016.algorithm.features;
+
+/**
+ * Created by pascal on 11.01.17.
+ */
+public class WordCount {
+}
