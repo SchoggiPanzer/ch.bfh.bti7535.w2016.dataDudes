6464f837ebcff53047676ed75d987b2dc3c6d234
Pascal Schmalz <pascal.schmalz@students.bfh.ch>
2017-01-11 14:38:08 +0100

    added WordCountFeature

--- src/main/java/ch/bfh/bti7535/w2016/algorithm/features/WordCount.java
+++ src/main/java/ch/bfh/bti7535/w2016/algorithm/features/WordCount.java
@@ -1,14 +1,29 @@
-/*
-* Copyright (c) 2017 Berner Fachhochschule, Switzerland.
-*
-* Project Smart Reservation System.
-*
-* Distributable under GPL license. See terms of license at gnu.org.
-*/
 package ch.bfh.bti7535.w2016.algorithm.features;
 
-/**
- * Created by pascal on 11.01.17.
- */
-public class WordCount {
+import ch.bfh.bti7535.w2016.data.Classification;
+import ch.bfh.bti7535.w2016.data.Document;
+import ch.bfh.bti7535.w2016.util.DocumentUtil;
+
+import java.util.List;
+
+
+public class WordCount extends AbstractFeature {
+
+
+    @Override
+    public double test(Document document) {
+        return DocumentUtil.countWords(document);
+    }
+
+    @Override
+    public double train(List<Document> documents, Classification classification){
+        double WordCount= 0;
+        for (Document doc : documents) {
+            if (doc.getGoldStandard().equals(classification)) {
+                WordCount += DocumentUtil.countWords(doc);
+            }
+        }
+
+        return WordCount;
+    }
 }
