c1cb89887b9bc28be24c00b75f86abec02fb0875
Pascal Zingg <pazingg@gmail.com>
2017-01-17 10:16:12 +0100

    Cosmetic

--- python/nb.py
+++ python/nb.py
@@ -5,22 +5,24 @@ from sklearn.pipeline import FeatureUnion
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from nltk.corpus import stopwords
-# import random
 import pandas as pd
 import nltk
 from stemmer import tokenize_and_stem
 
 
-nltk.download('all')
+# Downloads the nltk data
+nltk.download('stopwords')
 
+# Import the review data with pandas
 data = pd.read_csv("data.csv", sep=';', names=['sentiment', 'text'], encoding='ISO-8859-1')
-# random.shuffle(data)
-# print(data)
 
-# Feature definitions
+# Simple stop words feature
 count_feature = CountVectorizer(stop_words='english')
+
+# Stemmer feature
 stemmer_feature = CountVectorizer(tokenizer=tokenize_and_stem)
 
+# Stop words feature with TF-IDF
 stopset = set(stopwords.words('english'))
 tfidf_feature = TfidfVectorizer(use_idf=True, lowercase=True, stop_words=stopset)
 
@@ -31,18 +33,21 @@ feature_pipeline = FeatureUnion([
     ('tfidf', tfidf_feature),
 ], n_jobs=1)
 
+# Fit the data (x dependent variable, y independent variable)
 x = feature_pipeline.fit_transform(data.text)
 y = data.sentiment
 
 # Split the data into test and training sets
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-# Fit a naive bayes model to the training data.
-# This will train the model using the word counts we computer, and the existing classifications in the training set.
+# Train the model
 nb = MultinomialNB()
 nb.fit(x_train, y_train)
 
+# Calculate the results
 actual = y_test
 predictions = nb.predict(x_test)
-score = metrics.roc_auc_score(actual, predictions)
-print("Score: {0} %".format(score * 100))
+# score = metrics.roc_auc_score(actual, predictions)
+# print("Score: {0} %".format(score * 100))
+accuracy = metrics.accuracy_score(actual, predictions)
+print("Accuracy: {0} %".format(accuracy * 100))
